4f93c81801ce370af0ed98e5ddd8fa7d45691f1e
Linus Buddrus <buddruslinus@gmail.com>
2023-06-27 12:38:44 +0200

    refactor evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,26 +1,49 @@
 import torch
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, precision_recall_fscore_support, accuracy_score
 
 import config as cfg
 
-def compute_accuracy(model, dataloader):
-    correct, num_examples = 0, 0
-    for images, image_seq_len, label in dataloader:
-        images = images.to(cfg.DEVICE)
-        label = label.to(cfg.DEVICE)
-        logits = model((images, image_seq_len))
-        _, predicted = torch.max(logits, 1)
-        num_examples += logits.size(0) # batch size
-        correct += (predicted == label).sum()
-    return correct / num_examples * 100
-
-def compute_confusion_matrix(model, dataloader):
+@torch.no_grad()
+def compute_predicted_actual(model, dataloader):
     predicted_list, actual_list = [], []
     for images, image_seq_len, label in dataloader:
         images = images.to(cfg.DEVICE)
         label = label.to(cfg.DEVICE)
         logits = model((images, image_seq_len))
         _, predicted = torch.max(logits, 1)
-        predicted_list.extend(predicted.to("cpu"))
-        actual_list.extend(label.to("cpu"))
+        predicted_list.extend(predicted.cpu().numpy().tolist())
+        actual_list.extend(label.cpu().numpy().tolist())
+    return predicted_list, actual_list
+
+def compute_accuracy(model, dataloader):
+    predicted_list, actual_list = compute_predicted_actual(model, dataloader)
+    return accuracy_score(actual_list, predicted_list) * 100
+
+def compute_confusion_matrix(model, dataloader):
+    predicted_list, actual_list = compute_predicted_actual(model, dataloader)
     return confusion_matrix(actual_list, predicted_list)
+
+def compute_precision_recall_fscore(model, dataloader):
+    predicted_list, actual_list = compute_predicted_actual(model, dataloader)
+    return precision_recall_fscore_support(actual_list, predicted_list, average=None) 
+
+def compute_metrics(model, dataloader, metrics):
+    values = {}
+    predicted_list, actual_list = compute_predicted_actual(model, dataloader)
+
+    if "accuracy" in metrics:
+        values["accuracy"] = accuracy_score(actual_list, predicted_list) * 100
+
+    if "confusion_matrix" in metrics:
+        values["confusion_matrix"] = confusion_matrix(actual_list, predicted_list)
+
+    if "precision" in metrics:
+        values["precision"] = precision_score(actual_list, predicted_list, average=None)
+
+    if "recall" in metrics:
+        values["recall"] = recall_score(actual_list, predicted_list, average=None)
+
+    if "fscore" in metrics:
+        values["fscore"] = f1_score(actual_list, predicted_list, average=None)
+
+    return values
